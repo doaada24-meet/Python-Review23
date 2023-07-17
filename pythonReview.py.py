933ec55f933a2a8e7e3bb14fe773ce79db3596c3
doaada24-meet <139681641+doaada24-meet@users.noreply.github.com>
2023-07-17 11:18:15 +0300

    I finished the lab hoho!

---
+++ pythonReview.py.py
@@ -0,0 +1,27 @@
+def create_youtube_video(title,description):
+	youtubevideo={"title":title,"description":description,"likes":0,"dislikes":0,"comments":{}}
+	return youtubevideo
+def like(youtubevideo):
+	if "likes" in youtubevideo:
+		youtubevideo["likes"]+=1
+
+	return youtubevideo
+
+def dislikes(youtubevideo):
+	if "dislikes" in youtubevideo:
+		youtubevideo["dislikes"]+=1
+	return youtubevideo
+
+def add_comment(youtubevideo,username,comment_text)
+     comments["username"]=comment_text
+     return comments
+video=create_youtube_video{"first day in meet","in this video i filmed some clips of my first day in meet i hope you enjoy it :)"}
+ 
+
+
+
+
+	
+ 
+
+ 
